696809147ee0e4cec0d5bd33f6dd7e078902e6a4
KKM <skstr3182@snu.ac.kr>
2021-06-16 01:56:25 +0900

    Array refactored to be safe

--- NuDeal/source/Main.cpp
+++ NuDeal/source/Main.cpp
@@ -4,15 +4,18 @@
 #include "Input.h"
 #include "HardCodeParam.h"
 #include "XS.h"
-#include "Array.hpp"
+#include "Array.h"
 #include "MPI.hpp"
 #include "OMP.hpp"
+
+template class LinPack::Array_t<int>;
+template <typename T> using Array_t = LinPack::Array_t<T>;
 
 int main(int argc, char *argv[])
 {
 	MPI::Init(&argc, &argv);	
 	MPI::Configure_cuda_types();
-	
+
 	//LinPack::Array_t<double> a;
 	//LinPack::Array_t<double> b;
 	//LinPack::Array_t<int> c;
